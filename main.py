eea94b50240dd6c9b6b00b55cb6c4f92a9115b1e
Diundina_Maria <mary.diundina@gmail.com>
2020-10-23 17:51:06 +0500

    Add board, const, window, numb.

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QGridLayout, QPushButton, QApplication)
+from hitori import const
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+        self.setWindowTitle('Hitori')
+        height = const.BOARD_SIZE[0] * const.CELL_SIZE
+        weight = const.BOARD_SIZE[1] * const.CELL_SIZE
+        self.setGeometry(const.WINDOW_SIZE[0], const.WINDOW_SIZE[1],
+                         height, weight)
+        self.show()
+
+    def init_ui(self):
+        file_name = 'numb.txt'
+        with open(file_name) as f:
+            numbers = f.read()
+            numbers = numbers.split()
+
+        positions = [(i, j) for i in range(const.BOARD_SIZE[0])
+                     for j in range(const.BOARD_SIZE[1])]
+
+        for pos, number in zip(positions, numbers):
+            btn = QPushButton(number, self)
+            btn.setGeometry(pos[0] * const.CELL_SIZE, pos[1] * const.CELL_SIZE,
+                            const.CELL_SIZE, const.CELL_SIZE)
+            btn.setStyleSheet("QPushButton { background-color: grey }"
+                              "QPushButton:pressed { background-color: red }"
+                              "QPushButton")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec_())

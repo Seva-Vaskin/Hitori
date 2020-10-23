19d12969affaab483aa066a92de709636a222fd6
Diundina_Maria <mary.diundina@gmail.com>
2020-10-23 22:44:07 +0500

    Add MyButton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QGridLayout, QPushButton, QApplication)
+import enum
 from hitori import const
 
 
@@ -12,7 +13,6 @@ class Window(QWidget):
         weight = const.BOARD_SIZE[1] * const.CELL_SIZE
         self.setGeometry(const.WINDOW_SIZE[0], const.WINDOW_SIZE[1],
                          height, weight)
-
         file_name = 'numb.txt'
         with open(file_name) as f:
             numbers = f.read()
@@ -22,13 +22,27 @@ class Window(QWidget):
                      for j in range(const.BOARD_SIZE[1])]
 
         for pos, number in zip(positions, numbers):
-            btn = QPushButton(number, self)
+            btn = MyButton(number, self)
             btn.setGeometry(pos[0] * const.CELL_SIZE, pos[1] * const.CELL_SIZE,
                             const.CELL_SIZE, const.CELL_SIZE)
-            btn.setStyleSheet("QPushButton { background-color: grey }"
-                              "QPushButton:pressed { background-color: red }"
-                              "QPushButton")
+            btn.setStyleSheet("QPushButton { background-color: %s }"
+                              % const.Color.BLUE)
         self.show()
+
+
+class MyButton(QPushButton):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.color = const.Color.BLUE
+        self.clicked.connect(self.click)
+
+    def click(self):
+        if self.color in (const.Color.GREY, const.Color.GREY):
+            self.color = const.Color.WHITE
+        else:
+            self.color = const.Color.GREY
+        self.setStyleSheet("QPushButton { background-color: %s }" % self.color)
 
 
 if __name__ == '__main__':
